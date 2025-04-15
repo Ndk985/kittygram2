82cd96a4f81dd2b843738f7fa127590452c62738
Ndk985 <118891786+Ndk985@users.noreply.github.com>
2025-04-15 13:39:30 +0300

    kittygram2_v.1.0

--- cats/views.py
+++ cats/views.py
@@ -9,6 +9,18 @@ class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class AchievementViewSet(viewsets.ModelViewSet):
+    queryset = Achievement.objects.all()
+    
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
